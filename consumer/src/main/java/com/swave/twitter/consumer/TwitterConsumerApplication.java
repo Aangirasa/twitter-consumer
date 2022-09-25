c71ab68cf7852afb816c81f190122c9fa3f06419
aangirasa <aangirasa78@gmail.com>
2022-09-25 19:36:32 +0530

    init

---
+++ consumer/src/main/java/com/swave/twitter/consumer/TwitterConsumerApplication.java
@@ -0,0 +1,11 @@
+package com.swave.twitter.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterConsumerApplication.class);
+    }
+}
